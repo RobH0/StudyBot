6bed03cd4b24ff4e900f00ce29985f9d73dcfbfe
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-12 14:31:05 -0700

    User now prompted to enter a URL of a youtube video they want to have summarized.

--- study_bot.py
+++ study_bot.py
@@ -1,11 +1,31 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
 
-video_transcript = YouTubeTranscriptApi.get_transcript("1ENiVwk8idM")
-text_only_transcript = ''
-for line in video_transcript:
-    text_only_transcript += ' ' + line['text']
 
+def get_video_id():
+    url = str(input("Enter a URL of a Youtube video you want to take notes on: "))
+    if "youtube.com/watch?v=" in url:
+        print("Valid url")
+        start_index = url.find('=') + 1
+        end_index= start_index+11
+        id = url[start_index:end_index]
+        print("id " + str(id))
+        return id
+    else:
+        print("Invalid URL. Must be a link to a youtube video.")
+        return False
 
-text_only_transcript = text_only_transcript.replace("\n", "")
-print(text_only_transcript)
+def download_transcript(video_id):
+    video_transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    text_only_transcript = ''
+    for line in video_transcript:
+        text_only_transcript += ' ' + line['text']
+        
+    return text_only_transcript
+
+if __name__ == "__main__":
+    video_id = get_video_id()
+    if video_id != False:
+        text_only_transcript = download_transcript(video_id)
+        print(text_only_transcript)
+    
